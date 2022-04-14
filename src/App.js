302049b2563fb7c8329fd6e99ac0a81ad6cf301c
Byakuya91 <nganti91@yahoo.com>
2022-04-14 12:24:14 -0400

    I've successfully created the AddEntryForm component and coded out a partially working form and button.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries";
+import AddEntryForm from "./Components/AddEntry/AddEntryForm";
 
 function App() {
   const [entires, setEntries] = useState([{ weight: 175, date: "11-23-2022" }]);
   return (
     <div>
       <DisplayEntries parentEntries={entires} />
+      <AddEntryForm />
     </div>
   );
 }
