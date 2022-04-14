d99bae0bd30e21a57c5e6f8e825d6223615499f9
Byakuya91 <nganti91@yahoo.com>
2022-04-14 16:50:20 -0400

    I finished styling the weight tracker and it is working. Tutorial is successfully completed and thus I will do the following: study it for my next project.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries";
 import AddEntryForm from "./Components/AddEntry/AddEntryForm";
 import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
-
+import "./App.css";
 function App() {
   const [entires, setEntries] = useState([{ weight: 175, date: "11-23-2022" }]);
 
@@ -11,10 +11,25 @@ function App() {
     setEntries(tempEntries);
   }
   return (
-    <div>
-      <DisplayEntries parentEntries={entires} />
-      <AddEntryForm addEntryProperty={addNewEntry} />
-      <EntriesChartTracker parentEntries={entires} />
+    <div className="container-fluid">
+      <div className="row">
+        <h3 style={{ margin: "1em" }}>
+          Weight <small className="text-muted">Tracker</small>
+        </h3>
+        <div className="col-md-6">
+          <div className="border-box">
+            <DisplayEntries parentEntries={entires} />
+          </div>
+          <div className="border-box">
+            <AddEntryForm addEntryProperty={addNewEntry} />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="border-box">
+            <EntriesChartTracker parentEntries={entires} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
